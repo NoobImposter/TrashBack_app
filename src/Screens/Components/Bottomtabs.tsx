8fadabaca6718403b177c7a3b3d4c1ad476b0ecb
Muhammad Ali <muhammadalifora080222@gmail.com>
2025-11-14 15:12:05 +0500

    Start

---
+++ src/Screens/Components/Bottomtabs.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+import marketplaceIndex from '../Marketplace'
+import HomeIndex from '../Home'
+import RecycleIndex from '../Recycle'
+const Bottomtabs = () => {
+  return (
+    <View>
+      <Text>Bottomtabs</Text>
+    </View>
+  )
+}
+
+export default Bottomtabs
---
+++ src/Screens/Marketplace/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const marketplaceIndex = () => {
+  return (
+    <View>
+      <Text>marketplaceIndex</Text>
+    </View>
+  )
+}
+
+export default marketplaceIndex
